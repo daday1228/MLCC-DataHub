8e9c2dc46e23f7faa8c459fa3c3e03d8de18257f
Dayeong Kim <enospla02@gmail.com>
2026-04-19 00:34:20 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -63,7 +63,6 @@ function showForm() {
   if (!area) return;
 
   area.innerHTML = "";
-
   const user = sessionStorage.getItem("user") || "";
 
   /*******************
@@ -71,19 +70,18 @@ function showForm() {
    *******************/
   if (c === "surface") {
     area.innerHTML = `
-      <div class="form-card">
-        <div class="form-title">표면개질 공정 – 기본 정보</div>
-
+      <div class="section-wrapper">
+        <div class="section-label">기본 정보</div>
         <div class="form-grid">
           <div class="input-box"><label>실험일</label><input type="date"></div>
           <div class="input-box"><label>원료 ID</label><input type="text"></div>
           <div class="input-box"><label>실험자</label><input type="text" value="${user}" readonly></div>
           <div class="input-box"><label>표면개질 ID</label><input type="text"></div>
         </div>
+      </div>
 
-        <hr><br>
-
-        <div class="form-title">공정 조건</div>
+      <div class="section-wrapper">
+        <div class="section-label">공정 조건</div>
         <div class="form-grid">
           <div class="input-box"><label>원료 양</label><input type="text"></div>
           <div class="input-box"><label>개질제 양</label><input type="text"></div>
@@ -91,16 +89,20 @@ function showForm() {
           <div class="input-box"><label>반응 온도</label><input type="text"></div>
           <div class="input-box"><label>반응 시간</label><input type="text"></div>
         </div>
+      </div>
 
-        <hr><br>
-
-        <div class="form-title">측정 물성</div>
+      <div class="section-wrapper">
+        <div class="section-label">측정 물성</div>
         <div class="form-grid">
           <div class="input-box"><label>Zeta Potential</label><input type="text"></div>
           <div class="input-box"><label>체인 길이</label><input type="text"></div>
           <div class="input-box"><label>Density</label><input type="text"></div>
           <div class="input-box"><label>입도 분포</label><input type="text"></div>
         </div>
+      </div>
+
+      <div style="text-align:right; margin-top:30px;">
+        <button class="save-btn">저장하기</button>
       </div>
     `;
   }
@@ -110,19 +112,18 @@ function showForm() {
    *******************/
   else if (c === "paste") {
     area.innerHTML = `
-      <div class="form-card">
-        <div class="form-title">페이스트화 공정 – 기본 정보</div>
-
+      <div class="section-wrapper">
+        <div class="section-label">기본 정보</div>
         <div class="form-grid">
           <div class="input-box"><label>실험일</label><input type="date"></div>
           <div class="input-box"><label>표면개질 ID</label><input type="text"></div>
           <div class="input-box"><label>실험자</label><input type="text" value="${user}" readonly></div>
           <div class="input-box"><label>페이스트화 ID</label><input type="text"></div>
         </div>
+      </div>
 
-        <hr><br>
-
-        <div class="form-title">공정 조건</div>
+      <div class="section-wrapper">
+        <div class="section-label">공정 조건</div>
         <div class="form-grid">
           <div class="input-box"><label>배합비</label><input type="text"></div>
           <div class="input-box"><label>분산제</label><input type="text"></div>
@@ -131,15 +132,19 @@ function showForm() {
           <div class="input-box"><label>혼합 시간</label><input type="text"></div>
           <div class="input-box"><label>혼합 온도</label><input type="text"></div>
         </div>
+      </div>
 
-        <hr><br>
-
-        <div class="form-title">페이스트 측정 물성</div>
+      <div class="section-wrapper">
+        <div class="section-label">측정 물성</div>
         <div class="form-grid">
           <div class="input-box"><label>Viscosity</label><input type="text"></div>
           <div class="input-box"><label>Thixotropic Index</label><input type="text"></div>
           <div class="input-box"><label>점탄성</label><input type="text"></div>
         </div>
+      </div>
+
+      <div style="text-align:right; margin-top:30px;">
+        <button class="save-btn">저장하기</button>
       </div>
     `;
   }
@@ -149,29 +154,28 @@ function showForm() {
    *******************/
   else if (c === "sheet") {
     area.innerHTML = `
-      <div class="form-card">
-        <div class="form-title">시트화 공정 – 기본 정보</div>
-
+      <div class="section-wrapper">
+        <div class="section-label">기본 정보</div>
         <div class="form-grid">
           <div class="input-box"><label>실험일</label><input type="date"></div>
           <div class="input-box"><label>페이스트화 ID</label><input type="text"></div>
           <div class="input-box"><label>실험자</label><input type="text" value="${user}" readonly></div>
           <div class="input-box"><label>시트화 ID</label><input type="text"></div>
         </div>
+      </div>
 
-        <hr><br>
-
-        <div class="form-title">공정 조건</div>
+      <div class="section-wrapper">
+        <div class="section-label">공정 조건</div>
         <div class="form-grid">
           <div class="input-box"><label>캐스팅 두께</label><input type="text"></div>
           <div class="input-box"><label>캐스팅 속도</label><input type="text"></div>
           <div class="input-box"><label>건조 온도</label><input type="text"></div>
           <div class="input-box"><label>건조 시간</label><input type="text"></div>
         </div>
+      </div>
 
-        <hr><br>
-
-        <div class="form-title">측정 물성</div>
+      <div class="section-wrapper">
+        <div class="section-label">측정 물성</div>
         <div class="form-grid">
           <div class="input-box"><label>Zeta Potential</label><input type="text"></div>
           <div class="input-box"><label>Stability</label><input type="text"></div>
@@ -180,6 +184,10 @@ function showForm() {
           <div class="input-box"><label>바인더 분포 균일도</label><input type="text"></div>
           <div class="input-box"><label>표면조도 (Ra, Rpk)</label><input type="text"></div>
         </div>
+      </div>
+
+      <div style="text-align:right; margin-top:30px;">
+        <button class="save-btn">저장하기</button>
       </div>
     `;
   }
